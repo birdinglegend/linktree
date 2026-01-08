d7d773960fd153247b4d7a30a96c997cebe52d87
Nyoni Safaris <monkeygenesafaris@gmail.com>
2026-01-08 11:21:25 +0000

    now?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        home: path.resolve(__dirname, 'index.html'),
+      }
+    }
+  }
 })
